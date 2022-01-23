ef457b3a767bba620c7f3a12467ad130f539bd96
Chummy <57281249+Jimmy01240397@users.noreply.github.com>
2022-01-23 17:56:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get install byobu
 
-sed -i "s/\[ -n \"\$SHELL\" -a -x \"\$SHELL\" \] \&\& exec \"\$SHELL\" || exec \/bin\/sh/\[ -n \"\$SHELL\" -a -x \"\$SHELL\" \] \&\& exec \"\$SHELL\" --login || exec \/bin\/sh --login/g" /bin/byobu-shell
+sed -i "s/\[ -n \"\$SHELL\" -a -x \"\$SHELL\" \] \&\& exec \"\$SHELL\" || exec \/bin\/sh/\[ -n \"\$SHELL\" -a -x \"\$SHELL\" \] \&\& exec \"\$SHELL\" --login || exec \/bin\/sh --login/g" /usr/bin/byobu-shell
 sed -i "s/set -g default-command \$SHELL/set -g default-command \"\$SHELL --login\"/g" /usr/share/byobu/profiles/tmux
