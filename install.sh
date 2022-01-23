834dd12f2fe886420d53ccb1cd94060eda952c94
Chummy <57281249+Jimmy01240397@users.noreply.github.com>
2022-01-23 16:42:07 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ echo "then" >> /etc/profile.d/byobu.sh
 echo "    _byobu_sourced=1 TMUX= . /usr/bin/byobu-launch" >> /etc/profile.d/byobu.sh
 echo "fi" >> /etc/profile.d/byobu.sh
 
-sed -i "s/[ -n \"\$SHELL\" -a -x \"\$SHELL\" ] && exec \"\$SHELL\" || exec \/bin\/sh/[ -n \"\$SHELL\" -a -x \"\$SHELL\" ] && exec \"\$SHELL\" --login || exec \/bin\/sh --login/g" /bin/byobu-shell
+sed "s/[ -n \"\$SHELL\" -a -x \"\$SHELL\" ] && exec \"\$SHELL\" || exec \/bin\/sh/[ -n \"\$SHELL\" -a -x \"\$SHELL\" ] && exec \"\$SHELL\" --login || exec \/bin\/sh --login/g" /bin/byobu-shell
 sed -i "s/set -g default-command \$SHELL/set -g default-command \"\$SHELL --login\"/g" /usr/share/byobu/profiles/tmux
